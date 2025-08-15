55a949a523d4eb147c1ca2e826e9d7a5a923772d
arunp <arunprajapati9825@gmail.com>
2025-08-15 18:19:18 +0530

    added longest sub arr

---
+++ leetcode/lc_326.py
@@ -0,0 +1,79 @@
+import math
+
+
+class Solution:
+    def isPowerOfThree_iterative(self, n: int) -> bool:
+        '''
+        time complexity O(logn of base 3) and  space complexity O(1) 
+        :param n: 
+        :return: 
+        '''
+        if n <= 0:
+            return False
+
+        # I'm going to divide by 3 until
+        # it is divisible by 3 if final num is 1 then it is power of 3 else not
+        while n % 3 == 0:
+            n = n / 3
+        return n == 1 if True else False
+
+    def isPowerOfThree_recursive(self, n: int) -> bool:
+        '''
+         time complexity O(logN of base 3) same space complexity
+        :param n: 
+        :return: 
+        '''
+        if n <= 0:
+            return False
+
+        if n == 1:
+            return True
+
+        # I'm going to divide by 3 until
+        # it is divisible by 3 if final num is 1 then it is power of 3 else not
+        if n % 3 != 0:
+            return False
+        return self.isPowerOfThree_recursive(n // 3)
+
+    def isPowerOfThree_log(self, n: int) -> bool:
+        '''
+        time complexity O(1)
+        space complexity O(1)
+        :param n:
+        :return:
+        '''
+        if n <= 0:
+            return False
+        # how we are going to log to check
+        # as we know n is power of 3
+        # if 3^x==n or x==log(n)/log(3) is a valid integer then it is a power of 3
+        # in java log(n)/log(3) is written as Math.log(n)/Math.log(3) we stored them in Double type
+        x = math.log(n, 3)
+        y = int(round(x))
+        return abs(x - y) < 1e-12
+
+    def isPowerOfThree(self, n: int) -> bool:
+        # max integer is 2^31-1
+        # max integer that is power of 3 is 1162261467 or 3^19
+        if n <= 0:
+            return False
+        max_power_of_3 = math.pow(3, 19)
+        return max_power_of_3 % 3 == 0
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    n = 243
+    print("Iterative solution")
+    result: bool = sol.isPowerOfThree_iterative(n)
+    print(f"Result {result}")
+    print("Recursive solution")
+    result: bool = sol.isPowerOfThree_recursive(n)
+    print(f"Result {result}")
+    print("Math log solution")
+    result: bool = sol.isPowerOfThree_log(n)
+    print(f"Result {result}")
+    print("direct solution")
+    result: bool = sol.isPowerOfThree(n)
+    print(f"Result {result}")
+
---
+++ problems/longestSubarray.py
@@ -0,0 +1,63 @@
+class Solution:
+    """
+    Problem Statement:Given an array arr[] containing integers and an integer k,
+    your task is to find the length of the longest subarray where the sum of its
+    elements is equal to the given value k.
+    If there is no subarray with sum equal to k, return 0.
+    """
+
+    def longestSubarray_bruteForce(self, arr, k):
+        '''
+        Time Complexity=O(N^2)
+        Space Complexity= O(N)
+        :param arr:
+        :param k:
+        :return:
+        '''
+        n = len(arr)
+        max_len = 0
+
+        for left in range(n):
+            curr_sum = 0
+            for right in range(left, n):
+                curr_sum += arr[right]
+                if curr_sum == k:
+                    max_len = max(max_len, right - left + 1)
+        return max_len
+
+    def longestSubarray_better(self, arr, k):
+        '''
+        Time Complexity=O(N+N) ~O(2N) ~O(N)
+        Space Complexity= O(1)
+        :param arr:
+        :param k:
+        :return:
+        '''
+        n = len(arr)
+        left, right = 0, 0
+        max_len = 0
+        curr_sum = 0
+        prefix_sum_map = {0: -1}
+        while right < n:
+            curr_sum += arr[right]
+
+            if (curr_sum - k) in prefix_sum_map:
+                max_len = max(max_len, right - prefix_sum_map[curr_sum - k])
+
+            if curr_sum not in prefix_sum_map:
+                prefix_sum_map[curr_sum] = right
+
+            right += 1
+        return max_len
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    arr = [10, 5, 2, 7, 1, -10]
+    k = 15
+    print("Brute Force Solution")
+    result = sol.longestSubarray_bruteForce(arr, k)
+    print("Result:Brute Force", result)
+    print("Better Solution")
+    result = sol.longestSubarray_better(arr, k)
+    print("Result:Better", result)
